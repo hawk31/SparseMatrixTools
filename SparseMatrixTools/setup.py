f1424df15832b7960d6643b2b8d4a53744fe7dda
hawk31 <hawk31@gmail.com>
2015-02-15 15:32:36 +0100

    First commit
    Publish code

---
+++ SparseMatrixTools/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 15 15:27:51 2015
+
+@author: Jose
+"""
+
+from setuptools import setup
+
+setup(name='SparseMatrixTools',
+      version='0.1',
+      description='Sparse Matrix Operations tools',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
